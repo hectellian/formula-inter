e3ea6c08559d3bf906ccaf51c45ac8b9daed8c05
Christoforou Anthony <christoforouanthony@gmail.com>
2022-11-17 12:23:16 +0100

    added line by line file reading

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 
 mod utils;
 
@@ -8,22 +11,40 @@ use crate::utils::eval::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: {} \"file.fi\"", args[0]);
+        return;
+    }
+    let filename = &args[1];
+    if let Ok(lines) = read_lines(filename) {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(ip) = line {
+                println!("{}", ip);
+                interpret(ip);
+            }
+        }
+    }
+}
 
-    let expression = String::from( &args[1..].concat() );
-    
-    let l = Lexer::from(expression);
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn interpret(algebric: String) -> EvalResult {
+    let l = Lexer::from(algebric);
     let t = l.tokenize().ok().unwrap();
-    println!("algebric expression: {:?}", t);
-
-    print!("\n");
+    println!("algebric expression: {:?}\n", t);
 
     let p = postfixe(t);
-    println!("postfixe expression: {:?}", p);
-
-    print!("\n");
+    println!("postfixe expression: {:?}\n", p);
 
     let e = eval(p);
     println!("evalued expression result: {:?}", e);
+    
+    e
 }
 
 #[cfg(test)]
