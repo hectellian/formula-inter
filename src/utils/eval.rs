18aa2d525852dba1d4437b4187e6eb150c4860bb
Christoforou Anthony <christoforouanthony@gmail.com>
2022-11-17 11:54:15 +0100

    fixed eval only floats and added error handling

--- src/utils/eval.rs
+++ src/utils/eval.rs
@@ -1,4 +1,36 @@
+#![allow(dead_code,unused_variables)]
+
 use crate::utils::lexer::*;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum EvalError {
+    /** Invalid or Unkown Token */
+    InvalidToken,
+
+    /** Expression is Empty */
+    InvalidExpression,
+
+    /** Division by zero */
+    InvalidOperator,
+
+    /** Invalid number of arguments */
+    InvalidOperand,
+
+    /** Identifier or Variable */
+    NonNumericalValue
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum EvalResult {
+    /** Integer result */
+    Integer(i32),
+
+    /** Float result */
+    Float(f32),
+
+    /** Error */
+    Error(EvalError)
+}
 
 fn calc(v1: f32, op: char, v2: f32) -> f32 {
     match op {
@@ -11,27 +43,38 @@ fn calc(v1: f32, op: char, v2: f32) -> f32 {
     }
 }
 
-pub fn eval(postfixe: Vec<Token>) -> Result<f32, &'static str> {
+pub fn eval(postfixe: Vec<Token>) -> EvalResult {
     let mut stack = Vec::new();
-
-    for element in postfixe {
-        match element {
-            TokenKind::Integer(v) => stack.push(v.parse().unwrap_or(0.0)),
-            TokenKind::Real(v) => stack.push(v.parse().unwrap_or(0.0)),
-            TokenKind::Operator{ raw , .. } => {
-                let op1 = stack.pop();
-                let op2 = stack.pop();
-                let res = calc(op2.unwrap_or(0.0), raw, op1.unwrap_or(0.0));
-                stack.push(res);
-
-                println!("calc: {:?} {:?} {:?} = {:?}", op2.unwrap_or(0.0), raw, op1.unwrap_or(0.0), res);
+    for token in postfixe {
+        match token {
+            Token::Operator{raw, kind} => {
+                if stack.len() < 2 {
+                    return EvalResult::Error(EvalError::InvalidExpression);
+                }
+                let v2 = stack.pop().unwrap();
+                let v1 = stack.pop().unwrap();
+                let result = calc(v1, raw, v2);
+                stack.push(result);
             },
-            TokenKind::EOF => break,
-            _ => return Err("Invalid Format: Numeric Postfixe Format Expected")
+            Token::Integer(s) => {
+                let v = s.parse::<f32>().unwrap();
+                stack.push(v);
+            },
+            Token::Real(s) => {
+                let v = s.parse::<f32>().unwrap();
+                stack.push(v);
+            },
+            Token::EOF => break,
+            Token::Identifier(_) => return EvalResult::Error(EvalError::NonNumericalValue),
+            _ => return EvalResult::Error(EvalError::InvalidToken)
         }
     }
-
-    let evaluated: f32 = stack.pop().unwrap_or(0.0);
-
-    Ok(evaluated)
+    if stack.len() != 1 {
+        return EvalResult::Error(EvalError::InvalidExpression);
+    }
+    let result = stack.pop().unwrap();
+    if result.trunc() == result {
+        return EvalResult::Integer(result as i32);
+    }
+    EvalResult::Float(result)
 }
