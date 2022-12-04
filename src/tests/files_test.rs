a7d4c4b59d26708f5ac9455021bb843d1527bb11
hectellian <47461889+hectellian@users.noreply.github.com>
2022-12-04 21:40:03 +0100

    Update files_test.rs

--- src/tests/files_test.rs
+++ src/tests/files_test.rs
@@ -11,16 +11,3 @@ fn test_open_wrong_path() {
         Err(e) => assert_eq!(e.raw_os_error(), Some(2))
     }
 }
-
-#[test]
-fn test_open_correct_path() {
-    let filename = "right.fi";
-    let content = read_from(filename);
-    match content {
-        Ok(content) => {
-            print!("{}", content.as_str());
-            assert_eq!(content, "i = 12+5*3;\nafficher i;\naff_ral;\n\n       ");
-        }
-        Err(e) => assert_eq!(e.raw_os_error(), Some(2))
-    }
-}
